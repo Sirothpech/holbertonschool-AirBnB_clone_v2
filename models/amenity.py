49a6bf2331bc94da5f102615b2100630d65ca3fc
Sirothpech <christophe.ngan@gmail.com>
2023-08-16 15:20:35 +0200

    Add: script that starts a Flask web application task 9 cities by states

--- models/amenity.py
+++ models/amenity.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """Amenity class"""
 
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary="place_amenity",
                                    viewonly=False)
