89103012b61d983d34deb965df4916eac66a9d59
Sirothpech <christophe.ngan@gmail.com>
2023-08-16 15:49:04 +0200

    FIx the pycodestyle on the file 8 cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -54,7 +54,9 @@ def states_list():
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    return render_template("8-cities_by_states.html", states=storage.all(State))
+    return render_template("8-cities_by_states.html",
+                           states=storage.all(State))
+
 
 @app.teardown_appcontext
 def teardown(e):
